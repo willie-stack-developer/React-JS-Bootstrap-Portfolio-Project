52aa8665b4eb153e92b934857d3db64e519dd564
willie cortez <wcortez11@gmail.com>
2022-02-18 08:20:29 -0500

    Bootstrap Navbar complete

---
+++ src/components/Navbar.js
@@ -0,0 +1,61 @@
+import React from "react";
+import logo from "../logo.png";
+//react font-awesome import
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+const Navbar = () => {
+  return (
+      <nav className="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#"><img className={logo} src={logo} alt="logo"/></a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                  data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                  aria-label="Toggle navigation">
+            <FontAwesomeIcon icon={faBars} style={{color: "#fff"}}/>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">About Me</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Services</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">My Work</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Portfolio</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Contacts</a>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                   data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a className="dropdown-item" href="#">Action</a></li>
+                  <li><a className="dropdown-item" href="#">Another action</a></li>
+                  <li>
+                    <hr className="dropdown-divider"/>
+                  </li>
+                  <li><a className="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled">Disabled</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+  )
+}
+
+export default Navbar
